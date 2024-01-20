748bb09b4d5cecd1a03d97152e06d401a28bca30
taliberlin <118308290+taliberlin@users.noreply.github.com>
2024-01-19 17:12:11 -0800

    improved user experience

--- src/index.js
+++ src/index.js
@@ -9,19 +9,15 @@ function generatePoem(response) {
 function waitForPoem(event) {
   event.preventDefault();
 
-  let userInstructions = document.querySelector("#user-instructions").value;
-
-  new Typewriter("#poem", {
-    strings: `Generating a poem about "${userInstructions}"...`,
-    autoStart: true,
-    delay: 10,
-    cursor: "",
-  });
+  let userInstructions = document.querySelector("#user-instructions");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating a poem about "${userInstructions.value}"...</div>`;
 
   let apiKey = "o599f3bbe3f722tbacc3ebf3032624a0";
   let context =
     "You are a romatic poem-generating AI that loves to write poems.";
-  let prompt = `Generate a poem about ${userInstructions}. Make sure the poem is 4 lines and each line is separated by <br />. Sign the poem at the end, NOT in the beginning, with "SheCodes AI" which is wrapped in a <strong> element`;
+  let prompt = `Generate a poem about ${userInstructions.value}. Make sure the poem is 4 lines and each line is separated by <br />. Sign the poem at the end, NOT in the beginning, with "SheCodes AI" which is wrapped in a <strong> element`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(generatePoem);
 }
